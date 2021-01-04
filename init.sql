48b88a938b9538c79b7209f8b6c17b0ca97c66ea
RobertMatlag <robert.matlag@gmail.com>
2021-01-04 12:07:19 +0100

    Add table album

--- init.sql
+++ init.sql
@@ -39,6 +39,17 @@ create table Studio
             references Address
 );
 
+create table Album
+(
+    album_id serial not null
+    constraint album_pkey primary key,
+    name varchar(100) not null,
+    date timestamp default now(),
+    studio_id integer not null
+        constraint fk_studio
+            references Studio
+);
+
 INSERT INTO public.Country (name) VALUES ('Poland');
 INSERT INTO public.Country (name) VALUES ('Russia');
 INSERT INTO public.Country (name) VALUES ('Germany');
@@ -50,3 +61,5 @@ INSERT INTO public.City (name) VALUES ('Kuzmice');
 INSERT INTO public.Address VALUES (1,'Lwowska', 56, 2, 3);
 
 INSERT INTO public.Studio VALUES (1,'Janusze', 1);
+
+INSERT INTO public.Album (name, studio_id) VALUES ('Pod ksiezycem', 1);
