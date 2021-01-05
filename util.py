faf69d42026a68443e78a6d3e61448de3c3ce449
marcinchaluda <chaluda@interia>
2021-01-05 11:22:17 +0100

    Add function to generate random date

---
+++ util.py
@@ -0,0 +1,14 @@
+import random
+import datetime
+
+
+# create function accepting a single parameter, the year as a four digit number
+def get_random_date():
+    year = random.randint(1945, 2000)
+    # try to get a date
+    try:
+        return datetime.datetime.strptime('{} {}'.format(random.randint(1, 366), year), '%j %Y')
+
+    # if the value happens to be in the leap year range, try again
+    except ValueError:
+        return get_random_date(year)
